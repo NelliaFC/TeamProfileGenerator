ee1a793de7113056342b1bfa95cfc4092f45a18b
NeoNeonilla <nelliafleurova@gmail.com>
2021-09-02 18:18:32 -0500

    Lib js files created

---
+++ tests/Engineer.test.js
@@ -0,0 +1,28 @@
+const { it, expect } = require("@jest/globals")
+const Engineer = require("../lib/Engineer")
+
+describe("Engineer", () => {
+    it( "Can evoke Engineer class", () => {
+        const e = new Engineer();
+        expect(typeof(e)).toBe("object");
+
+    });
+
+    
+});
+
+describe("getRole", () => {
+    it("getRole() should return \"Engineer\"", () => {
+        const testValue = "Engineer";
+        const e = new Engineer("Bob", 1, "test@test.com", "GitHub");
+        expect(e.getRole()).toBe(testValue);
+    });
+});
+ describe("getGithub", () => {
+     it("getGitHub()  should return \"github user\"" , () => {
+         const testValue = "gitHub";
+         const e = new Engineer("Bob", 1, "test@test.com", testValue);
+         expect(e.getGithub()).toBe(testValue);
+     });
+ });   
+ 
