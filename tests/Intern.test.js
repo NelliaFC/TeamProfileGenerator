28cfc33f3a1bae3f09fbff256b3893d775321779
NeoNeonilla <nelliafleurova@gmail.com>
2021-09-02 20:28:27 -0500

    changes to intern test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,13 +1,12 @@
 const { test, expect } = require("@jest/globals")
 const Intern = require("../lib/Intern")
 
-test("intern", () => {
-    it( "Can evoke Intern class", () => {
+test("intern() => {Can evoke Intern class", () => {
         const testValue = Intern
         const e = new Intern();
         expect(typeof(e)).toBe("object");
     });
-});
+
 
 test("getRole () => { should return \"Intern\"", () => {
         const testValue = "Intern";
@@ -17,6 +16,6 @@ test("getRole () => { should return \"Intern\"", () => {
     
 test("getSchool()   => {should return \"schoolName\"", () => {
     const testValue = "school";
-    const e = new Intern("Zoya", 2, "test@test.com", "school");
+    const e = new Intern("Zoya", 2, "test@test.com", "schoolName");
     expect(e.getSchool()).toBe(testValue);
 })
