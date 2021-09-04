00d400470144cf57d8d4343ab3eeb9dc26969475
NeoNeonilla <nelliafleurova@gmail.com>
2021-09-04 12:13:57 -0500

    completed tests for the program

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -4,21 +4,15 @@
  test("Intern", () => {
      
  })
-// test("intern() => {Can evoke Intern class", () => {
-//         const testValue = Intern
-//         const e = new Intern();
-//         expect(typeof(e)).toBe("object");
-//     });
+test("intern() => {Can evoke Intern class", () => {
+        const testValue = Intern
+        const e = new Intern();
+        expect(typeof(e)).toBe("object");
+    });
 
-
-// test("getRole () => { should return \"Intern\"", () => {
-//         const testValue = "Intern";
-//         const e = new Intern("Zoya", 2, "test@test.com", "school");
-//         expect(e.getSchool()).toBe(testValue);
-//     });
     
-// test("getSchool()   => {should return \"schoolName\"", () => {
-//     const testValue = "school";
-//     const e = new Intern("Zoya", 2, "test@test.com", "schoolName");
-//     expect(e.getSchool()).toBe(testValue);
-// })
+test("getSchool() => {should return \"school\"", () => {
+    const testValue = "school";
+    const e = new Intern("Zoya", 2, "test@test.com", "school");
+    expect(e.getSchool()).toBe(testValue);
+})
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,6 +1,15 @@
 const { test, expect } = require("@jest/globals")
 const Manager = require("../lib/Manager")
 
-test("Manager", () => {
-    
-})
+test("Manage can set office number", () => {
+    const testValue = 4;
+    const e = new Manager("Sunshine", 4, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+
+test("getRole() will return \"Manager\"", () => {
+    const testValue = Manager
+    const e = new Manager("Sunshine", 4, "test@test.com", testValue);
+    expect(typeof(e)).toBe("object")
+});
+
